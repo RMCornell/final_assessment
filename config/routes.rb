beafb434dbcff026cf0dea2b6a0a3415857060a5
Robert Cornell <robert.cornell78@gmai.com>
2016-01-20 10:37:02 -0700

    iteration one functioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get '/' => 'home#index'
+  root 'home#index'
+
+  # User Create Routes
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   resources :users
+
+  # User Login/Logout Routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+
 
 end
