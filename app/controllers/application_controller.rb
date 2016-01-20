cb42df9b3893bbe7f1ff02b0e59780939345bd86
Robert Cornell <robert.cornell78@gmai.com>
2016-01-20 15:31:01 -0700

    updating tasklists working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,4 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to '/login' unless current_user
   end
-
-  rescue_from 'ActiveRecord::InvalidForeignKey' do
-    redirect_to root_path
-  end
 end
